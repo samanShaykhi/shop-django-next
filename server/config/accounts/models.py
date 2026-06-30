edbcbd7490ff2fa9a889d8efe86bd6705a26a561
saman <saman@gmail.com>
2026-06-30 14:46:38 +0330

    debug

--- server/config/accounts/models.py
+++ server/config/accounts/models.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from versatileimagefield.fields import VersatileImageField
-from django.contrib.auth.models import UserManager as DjangoUserManager
-
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
 class Address(models.Model):
@@ -20,12 +19,24 @@ class Address(models.Model):
         return f"{self.receiver_name} {self.province_city}"
 
 
-class CustomUserManager(DjangoUserManager):
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Phone number is required")
+
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
-        return self._create_user(phone_number, None, password, **extra_fields)
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class User(AbstractUser):
